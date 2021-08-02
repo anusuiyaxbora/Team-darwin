0015e1c92c56afe9dbb57ba6b50821164da4fbd9
Hassan <ajulohassankehinde@gmail.com>
2021-08-02 23:42:39 +0100

    Delete First_Assignment_Hassan.R

--- First_Assignment_Hassan.R
+++
@@ -1,6 +0,0 @@
-my_details <- c("Name: AJULO, Hassan Kehinde", "Email: ajulohassankehinde@gmail.com", "Username: Hassan", "BioStack: Genomics")
-
-for (i in c(1,2,3,4)){
-	print(my_details[i])
-	i = i + 1
-}
