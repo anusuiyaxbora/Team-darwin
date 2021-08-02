d74d87111578a903d76da26f29c931fbea85cd05
Hassan <ajulohassankehinde@gmail.com>
2021-08-02 15:45:39 +0100

    Add files via upload

---
+++ First_Assignment_Hassan.R
@@ -0,0 +1,5 @@
+my_details <- c("Name: AJULO, Hassan Kehinde", "Email: ajulohassankehinde@gmail.com", "Username: Hassan", "BioStack: Genomics")
+i = 1
+for (i in c(1,2,3,4)){
+	print(my_details[i])
+}
