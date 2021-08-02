24084ddf1654ec3bee371b11e76752042d9e9bbd
utkarsha-05 <88287648+utkarsha-05@users.noreply.github.com>
2021-08-02 21:45:11 +0530

    First task by Utkarsha Pande - Team Darwin

---
+++ C_Utkarsha.c
@@ -0,0 +1,12 @@
+/******************************************************************************
+TASK 1 The Spreadsheet Project
+
+*****************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf(" * NAME : Utkarsha Pande\n * E-mail : utkarshanp@gmail.com \n * SLACK Username : @Utkarsha\n * LANGUAGE USED : C PROGRAMMING\n * BIOSTACK : TRANSCRIPTOMICS \n" );
+    return 0;
+}
