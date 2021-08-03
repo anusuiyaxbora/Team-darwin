911493828524d3022a9339542966bb8ce26b8823
Kay Kay Shain <88278330+KKShain@users.noreply.github.com>
2021-08-03 13:42:51 +0600

    Update Script_KKShain.R

--- Script_KKShain.R
+++ Script_KKShain.R
@@ -1,1 +1,9 @@
-cat(' Name: kay kay shain marma \n email: kkshainapu@gmail.com \n slack id: @kkshain \n Biostack: Genomics')
+name <- "kay kay shain marma"
+email <- "kkshainapu@gmail.com"
+language <- "R"
+biostack <- "Genomics"
+slackuser <- "@kkshain"
+data <- data.frame(Information=c(name, email, language, slackuser, biostack))
+rownames(data) <- c('NAME: ', 'E-MAIL: ', 'USED LANGUAGE: ', 'SLACK USERNAME: ', 'BIOSTACK: ')
+colnames(data) <- NULL
+print(data)
