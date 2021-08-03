00d1c64e9a558efe047ab3bfaecebd5c4392b092
laolanllx <64663278+laolanllx@users.noreply.github.com>
2021-08-03 07:07:42 -0400

    Submit my task one assignment in Python

---
+++ script_Emma.py
@@ -0,0 +1,12 @@
+name = 'Lanxin Liu'
+email = 'lanxin.liu@temple.edu'
+slackname = '@Emma'
+biostack = 'Genomics & Data Analytics'
+
+output = '{}\n{}\n{}\n{}\n'.format(name,email,slackname,biostack)
+print(output)
+
+# optional -- output to a file
+# f = open('task1.csv','w')
+# f.write(output)
+# f.close()
