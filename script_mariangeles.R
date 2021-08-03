56fda363bb45ec42d6bf9bfd497f1741bae991f4
Mariangeles Salas <46332211+MarianSalasL@users.noreply.github.com>
2021-08-03 13:06:04 -0400

    Update script_mariangeles.R

--- script_mariangeles.R
+++ script_mariangeles.R
@@ -2,4 +2,4 @@ Name<- 'Mariángeles Salas'
 Email<- 'mariangelescsl@gmail.com'
 Slack<- '@Mariangeles'
 Biostack<- 'Medicinal chemistry and cheminformatics'
-cat(Name, Email, Slack, Biostack)
+cat(paste(Name, Email, Slack, Biostack, sep = ','))
