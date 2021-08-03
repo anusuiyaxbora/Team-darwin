58ab7ce1c04b7d622491112ecd8ba2410755e704
anusuiyaxbora <88286275+anusuiyaxbora@users.noreply.github.com>
2021-08-03 23:42:36 +0530

    Add files via upload

---
+++ script_AnusuiyaBora.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Details {
+	public static void main (String[] args) {
+	    String name = "Anusuiya Bora";
+	    String email = "bora.anusuiya818@gmail.com";
+	    String username = "@AnusuiyaBora";
+	    String biostack = "Drug Development";
+	    System.out.println(name + "," + email + "," + username + "," + biostack);
+	}
+}
