a3b3fd3d06f2eb199e3c6944c5fcf92202796c1e
madhumitha-rsuresh <88226429+madhumitha-rsuresh@users.noreply.github.com>
2021-08-04 13:55:36 +0530

    Updating the solutions to new task given
    Two more requirements have been asked on 4.8.2021. They were twitter id and hamming distance. The code for those have been added.

--- script_Madhu.m
+++ script_Madhu.m
@@ -2,4 +2,7 @@ name ='Madhumitha';
 email='madhumitha.rsuresh@gmail.com';
 slackid='@Madhu';
 biostack='Drug Development';
-fprintf('%s,%s,%s,%s',name,email,slackid,biostack)
+twitterid = 'MadhumithaSure5';
+dist = length(twitterid) - length(slackid);
+hamming_dist = dist + sum(slackid ~= twitterid(1:length(slackid)));
+fprintf('%s,%s,%s,%s,%s,%d',name,email,slackid,biostack,twitterid,hamming_dist)
