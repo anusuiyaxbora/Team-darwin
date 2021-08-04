453a57f659ca93cf632b69a1a6637607b85d260e
laolanllx <64663278+laolanllx@users.noreply.github.com>
2021-08-04 00:37:02 -0400

    Change ‘\n’ to comma

--- script_Emma.py
+++ script_Emma.py
@@ -3,7 +3,7 @@ email = 'lanxin.liu@temple.edu'
 slackname = '@Emma'
 biostack = 'Genomics & Data Analytics'
 
-output = '{}\n{}\n{}\n{}\n'.format(name,email,slackname,biostack)
+output = name + ','+ email + ',' + slackname + ',' + biostack)
 print(output)
 
 # optional -- output to a file
