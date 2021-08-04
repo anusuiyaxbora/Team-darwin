90663db36a85ac2e7d7314ab961767100a504db6
madhumitha-rsuresh <88226429+madhumitha-rsuresh@users.noreply.github.com>
2021-08-04 15:25:48 +0530

    Adding @ in twitter id

--- script_Madhu.m
+++ script_Madhu.m
@@ -2,7 +2,7 @@ name ='Madhumitha';
 email='madhumitha.rsuresh@gmail.com';
 slackid='@Madhu';
 biostack='Drug Development';
-twitterid = 'MadhumithaSure5';
+twitterid = '@MadhumithaSure5';
 dist = length(twitterid) - length(slackid);
 hamming_dist = dist + sum(slackid ~= twitterid(1:length(slackid)));
 fprintf('%s,%s,%s,%s,%s,%d',name,email,slackid,biostack,twitterid,hamming_dist)
