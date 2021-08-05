26b2ce7d4bf31bb8ebd7edf54d779fd61295a304
Mcberners <88288379+Mcberners@users.noreply.github.com>
2021-08-05 19:59:02 +0100

    Add files via upload
    Submitting my task in Ruby Script

---
+++ main (1).rb
@@ -0,0 +1,7 @@
+name = "Ifeanyichukwu Owoh"
+email = "michaelberners27@gmail.com"
+username = "@Mcberners"
+biostack = "DRUG DEVELOPMENT"
+twitter = "@Owoh_M_I_B"
+hamming = "10"
+puts name + "," + email + "," + username + "," + biostack + "," + twitter + "," + hamming
