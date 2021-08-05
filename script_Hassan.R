901996f059d9fcf59f77a45a79d2793bca37936a
Hassan <ajulohassankehinde@gmail.com>
2021-08-05 17:57:34 +0100

    script_Hassan

--- script_Hassan.R
+++ script_Hassan.R
@@ -1,10 +1,10 @@
 name <- "AJULO Hassan Kehinde"
 email <- "ajulohassankehinde@gmail.com"
 biostack <- "Genomics"
-slackID_vector <- c('@','H','a','s','s','a','n')
-twitterHandle_vector <- c('@','H','a','s','s','a','n')
-slackID <- paste(slackID_vector, collapse = '')
-twitterHandle <- paste(twitterHandle_vector, collapse = '')
-hamming_dist <- sum(slackID_vector != twitterHandle_vector)
-my_details <- c(name,email,slackID,biostack,twitterHandle,hamming_dist)
+slack_username_vector <- c('@','H','a','s','s','a','n')
+twitter_handle_vector <- c('@','H','a','s','s','a','n','A','j','u','l','o')
+slack_username <- paste(slack_username_vector, collapse = '')
+twitter_handle <- paste(twitter_handle_vector, collapse = '')
+hamming_dist <- sum(slack_username_vector != twitter_handle_vector)
+my_details <- c(name,email,slack_username,biostack,twitter_handle,hamming_dist)
 cat(paste(my_details, collapse = ','))
