8291d20177e93f1c39413966cd8f58274083f76c
AyomFash <88287741+AyomFash@users.noreply.github.com>
2021-08-05 19:43:20 +0200

    Update script_Ayo123.R

--- script_Ayo123.R
+++ script_Ayo123.R
@@ -1,4 +1,11 @@
-my_details <-c("Fasemire Ayomide Samson","ayomidesamson14@gmail.com","@Ayo123","Transcriptomics")
-
+name <- "Fasemire Ayomide Samson"
+email <- "ayomidesamson14@gmail.com"
+biostack <- "Transcriptomics"
+p <- c ('@','A','y','o','1','2','3')
+q <- c('@','A','Y','O','M','I','D','E','6','8','5','9','1','2','4','8')
+slackID <- paste(p, collapse = '')
+Twitterhandle <- paste(q, collapse = '')
+hamming.dist <- sum(p != q)
+my_details <- c(name,email,slackID,biostack,Twitterhandle,hamming.dist)
 cat(paste(my_details, collapse = ','))
-
+"\n"
