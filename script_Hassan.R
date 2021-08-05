882a5ad1ee6730be7794c0892c3c89941e68fe3a
Hassan <ajulohassankehinde@gmail.com>
2021-08-05 18:06:17 +0100

    Update script_Hassan.R

--- script_Hassan.R
+++ script_Hassan.R
@@ -1,10 +1,10 @@
 name <- "AJULO Hassan Kehinde"
 email <- "ajulohassankehinde@gmail.com"
 biostack <- "Genomics"
-slack_username_vector <- c('@','H','a','s','s','a','n')
+slack_username_vector <- c('@','H','a','s','s','a','n', rep('',5))
 twitter_handle_vector <- c('@','H','a','s','s','a','n','A','j','u','l','o')
 slack_username <- paste(slack_username_vector, collapse = '')
 twitter_handle <- paste(twitter_handle_vector, collapse = '')
-hamming_distance <- sum(slack_username_vector != twitter_handle_vector)
-my_details <- c(name,email,slack_username,biostack,twitter_handle,hamming_distance)
+hamming_dist <- sum(slack_username_vector != twitter_handle_vector)
+my_details <- c(name,email,slack_username,biostack,twitter_handle,hamming_dist)
 cat(paste(my_details, collapse = ','))
