46f0ebe9aeeb037b4e3f9c1b673562e027c190ef
Akinsehinwa Samuel <88286541+Ifemide2@users.noreply.github.com>
2021-08-05 14:33:51 +0100

    First task

---
+++ script_Ifemide.py
@@ -0,0 +1,19 @@
+Name = "Akinsehinwa Samuel"
+Email = "akinsehinwa.sam@gmail.com"
+SlackID = "Ifemide2"
+Biostack = "Genomics"
+TwitterID = "ifemide4"
+
+def hamming_distance(a, b):
+    i = 0
+    count = 0
+
+    while (i < len (a)):
+        if a[i] != b[i]:
+            count += 1
+        i += 1
+    return count
+
+hamming_distance(SlackID, TwitterID)
+
+print(Name + "," , Email + "," , SlackID + "," , Biostack + "," , TwitterID + "," +  str (hamming_distance(SlackID, TwitterID)))
