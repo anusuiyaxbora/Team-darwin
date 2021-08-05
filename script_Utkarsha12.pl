c21868cd41de7217a87535043a1aa1601a5b581d
utkarsha-05 <88287648+utkarsha-05@users.noreply.github.com>
2021-08-05 19:50:30 +0530

    Update script_Utkarsha12.pl

--- script_Utkarsha12.pl
+++ script_Utkarsha12.pl
@@ -3,11 +3,10 @@
 $Name= 'Utkarsha Pande';
 $email= 'utkarshanp@gmail.com';
 $Slack_id = '@Utkarsha12';
-$Biostack = 'EPIGENOMICSt';
+$Biostack = 'EPIGENOMICS';
 $twitter= '@Utkar-sha50';
  
-print("$Name,$email,$Slack_id,$Biostack,$twitter,");
-
+# For Hamming_distance 
 $SLACK="@Utkarsha12";
 $TWITTER= "@Utkar-sha50";
 $hamming_dist=0;
@@ -15,4 +14,4 @@ for (my $i=0; $i<length $TWITTER; $i++)
      {
       ++$hamming_dist if substr($TWITTER, $i, 1) ne substr($SLACK, $i, 1);
      }
-print("$hamming_dist" )
+print("$Name,$email,$Slack_id,$Biostack,$twitter,$hamming_dist");
