52eac9b9ec80af152a4c6abec5498df7d3e66902
Mariangeles Salas <46332211+MarianSalasL@users.noreply.github.com>
2021-08-05 17:57:02 -0400

    Update script_mariangeles.R

--- script_mariangeles.R
+++ script_mariangeles.R
@@ -4,6 +4,7 @@ slack<- '@Mariangeles'
 biostack<- 'Medicinal chemistry and cheminformatics'
 twitter<- '@MarianSalasL'
 twitter_split <- c(strsplit(twitter, "")[[1]])
-slack_split <- strsplit(slack, "")[[1]]
+slack_split <- c(strsplit(slack, "")[[1]], '')
 hamming_distance <- sum(twitter_split != slack_split)
 cat(paste(name, email, slack, biostack, twitter, hamming_distance, sep = ','))
+
