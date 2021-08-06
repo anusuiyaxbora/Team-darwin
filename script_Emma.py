daba0415f76934aa490caec827f233de7ba29f2e
laolanllx <64663278+laolanllx@users.noreply.github.com>
2021-08-06 11:39:48 -0400

    Update script_Emma.py
    Add hamming distance

--- script_Emma.py
+++ script_Emma.py
@@ -2,11 +2,21 @@ name = 'Lanxin Liu'
 email = 'lanxin.liu@temple.edu'
 slackname = '@Emma'
 biostack = 'Genomics & Data Analytics'
+twitterid = 'llx0610'
 
-output = name + ','+ email + ',' + slackname + ',' + biostack
+def hamming_dis(a, b):
+    count = 0
+    output = 'Strings are not equal'
+    if len(a) != len(b):
+        return output
+    else:
+        for x, (i, j) in enumerate(zip(a,b)):
+            if i != j:
+                print(f'char not math{i,j} in {x}')
+            count += 1
+        return count
+
+hamming_distance = hamming_dis(slackname, twitterid)
+output = name + ','+ email + ',' + slackname + ',' + biostack + ',' + 'twitterid' + ',' + str(hamming_distance)
 print(output)
 
-# optional -- output to a file
-# f = open('task1.csv','w')
-# f.write(output)
-# f.close()
