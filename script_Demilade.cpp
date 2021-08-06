2801e356e6087a67d1b3a6e7cdda6c661aa28323
DemiAkinyemi <88286567+DemiAkinyemi@users.noreply.github.com>
2021-08-06 15:28:07 +0100

    Add files via upload

--- script_Demilade.cpp
+++ script_Demilade.cpp
@@ -1,14 +1,37 @@
 // HackBio Internship 2021
-
-#include <iostream>
+// Using C++ programming language to show my details and calculate the hamming distance between twitter and slack handle
+#include<bits/stdc++.h>
 using namespace std;
+// Hamming distance
+int hammingDist(char *str1, char *str2)
+{
+	int i = 0, count = 0;
+	while (str1[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			count++;
+		i++;
+	}
+	return count;
+}
+// main code to show my details and calculate the hamming distance between twitter and slack handle
+int main()
+{
+	char str1[] = "@Demilade";
+	char str2[] = "@de_milade";
 
-
-int main() {
-  cout << "Oluwademilade Akinyemi,";
+  cout << "Demilade Akinyemi,";
   cout << "akinyemi.o.ife@gmail.com,";
   cout << "@Demilade,";
-  cout << "Genomics";
-  return 0;
+  cout << "Genomics,";
+  cout << "@de_milade,";
+  cout << hammingDist (str1, str2);
+  
+	return 0;
 }
+
+
+
+
+
 
