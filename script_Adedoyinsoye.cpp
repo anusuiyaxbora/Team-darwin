2cc5627b07c734c94985372561574c32abb45434
adedoyinsoye <49322487+adedoyinsoye@users.noreply.github.com>
2021-08-06 15:28:50 +0100

    UPDATED SCRIPT

--- script_Adedoyinsoye.cpp
+++ script_Adedoyinsoye.cpp
@@ -1,14 +1,36 @@
 // HackBio Internship 2021
-
-#include <iostream>
+// TEAM DARWIN 
+#include<bits/stdc++.h>
 using namespace std;
 
+// Calculating Hamming Distance
+int hammingDist(char *str1, char *str2)
+{
+	int i = 0, count = 0;
+	while (str1[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			count++;
+		i++;
+	}
+	return count;
+}
 
-int main() {
-  cout << "Adedoyin Adesoye,";
-  cout << "adesoyeadedoyin@gmai.com,";
-  cout << "@Adedoyinsoye,";
-  cout << "Genomics";
+#include <iostream>
+
+int main() 
+{
+	// Let str1 = slack handle; str2 = twitter handle
+	char str1[] = "@adedoyinsoye";
+	char str2[] = "@Adedoyinsoye";
+	//FUNCTIONS
+  cout << " NAME: Adedoyin Adesoye, ";
+  cout << "EMAIL: adesoyeadedoyin@gmail.com, ";
+  cout << "SLACK HANDLE: @Adedoyinsoye, ";
+  cout << "BIOSTACK: Genomics, ";
+  cout << "TWITTER HANDLE: @adedoyinsoye, ";
+  cout << hammingDist (str1, str2);
+  
   return 0;
 }
 
