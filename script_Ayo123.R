4afae0033c5b242c5f693caa7f0a2cfc04537c14
AyomFash <88287741+AyomFash@users.noreply.github.com>
2021-08-06 16:28:19 +0200

    Update script_Ayo123.R

--- script_Ayo123.R
+++ script_Ayo123.R
@@ -1,11 +1,10 @@
 name <- "Fasemire Ayomide Samson"
 email <- "ayomidesamson14@gmail.com"
 biostack <- "Transcriptomics"
-p <- c ('@','A','y','o','1','2','3')
+p <- c ('@','A','y','o','1','2','3','*','*','*','*','*','*','*','*','*')
 q <- c('@','A','Y','O','M','I','D','E','6','8','5','9','1','2','4','8')
 slackID <- paste(p, collapse = '')
 Twitterhandle <- paste(q, collapse = '')
 hamming.dist <- sum(p != q)
 my_details <- c(name,email,slackID,biostack,Twitterhandle,hamming.dist)
 cat(paste(my_details, collapse = ','))
-
